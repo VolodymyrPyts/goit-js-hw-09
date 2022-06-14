69114bf3faba1f29623ee6b85fedf2c67047b583
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-14 14:27:27 +0300

    task#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+    bodyEl: document.querySelector("body"),
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+}
+let intervalId = null;
 
+refs.startBtn.addEventListener("click", onClickStart)
+refs.stopBtn.addEventListener("click", onClickStop)
+
+refs.stopBtn.setAttribute('disabled', true)
+
+function onClickStart() {
+    refs.stopBtn.removeAttribute('disabled', false)
+    refs.startBtn.setAttribute('disabled', true)
+    intervalId = setInterval(() => {
+        refs.bodyEl.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+}
+
+function onClickStop() {
+    refs.startBtn.removeAttribute('disabled', false)
+    refs.stopBtn.setAttribute('disabled', true)
+    clearInterval(intervalId) ;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
