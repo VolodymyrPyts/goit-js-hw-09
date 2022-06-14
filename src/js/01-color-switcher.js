46abbf3c59700570f817663e4be0e7504767f932
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-14 22:05:49 +0300

    task#2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,17 +2,21 @@ const refs = {
     bodyEl: document.querySelector("body"),
     startBtn: document.querySelector("[data-start]"),
     stopBtn: document.querySelector("[data-stop]"),
+    resetBtn: document.querySelector("[data-reset]"),
 }
 let intervalId = null;
 
 refs.startBtn.addEventListener("click", onClickStart)
 refs.stopBtn.addEventListener("click", onClickStop)
-
+refs.resetBtn.addEventListener("click", onClickReset)
 refs.stopBtn.setAttribute('disabled', true)
+refs.resetBtn.setAttribute('disabled', true)
+
 
 function onClickStart() {
     refs.stopBtn.removeAttribute('disabled', false)
     refs.startBtn.setAttribute('disabled', true)
+    refs.resetBtn.setAttribute('disabled', true)
     intervalId = setInterval(() => {
         refs.bodyEl.style.backgroundColor = getRandomHexColor()
     }, 1000);
@@ -20,6 +24,7 @@ function onClickStart() {
 
 function onClickStop() {
     refs.startBtn.removeAttribute('disabled', false)
+    refs.resetBtn.removeAttribute('disabled', false)
     refs.stopBtn.setAttribute('disabled', true)
     clearInterval(intervalId) ;
 }
@@ -29,3 +34,7 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onClickReset() {
+    refs.bodyEl.style.backgroundColor = "transparent";
+} 
