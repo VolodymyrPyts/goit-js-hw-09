471eb0a13db513274a04d356ccd29d970dcd6880
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-06-18 14:31:54 +0300

    task#2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,17 +1,7 @@
-const flatpickr = require("flatpickr");
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import Notiflix from 'notiflix';
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+require("flatpickr/dist/themes/material_blue.css");
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -30,4 +20,65 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+const refs = {
+  timePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysField: document.querySelector('[data-days]'),
+  hoursField: document.querySelector('[data-hours]'),
+  minutesField: document.querySelector('[data-minutes]'),
+  secondsField: document.querySelector('[data-seconds]'),
+  
+};
+
+refs.startBtn.setAttribute('disabled', true)
+refs.startBtn.addEventListener('click', onStartClick);
+
+const libraryFlatpickr = flatpickr(refs.timePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] > Date.now()) {
+      refs.startBtn.removeAttribute('disabled');
+    } else {
+      Notify.failure('Please choose a date in the future', {
+        position: 'rigth-top',
+        closeButton: true,
+      });
+    }
+  },
+});
+
+function onStartClick() {
+  refs.startBtn.setAttribute('disabled', true);
+  refs.timePicker.setAttribute('disabled', true);
+
+  const timer = setInterval(() => {
+    const deltaDate = libraryFlatpickr.selectedDates[0].getTime() - Date.now();
+
+    if (deltaDate > 0) {
+      renderTimer(convertMs(deltaDate));
+      refs.secondsField.classList.add('zero');
+    }
+    else {
+      clearInterval(timer);
+      refs.timePicker.removeAttribute('disabled');
+      refs.secondsField.classList.remove('zero');
+      Notify.info("Time's up!", { position: 'rigth-top' });
+    }
+  }, 1000);
+}
+
+function renderTimer({ days, hours, minutes, seconds }) {
+  refs.daysField.textContent = addLeadingZero(days);
+  refs.hoursField.textContent = addLeadingZero(hours);
+  refs.minutesField.textContent = addLeadingZero(minutes);
+  refs.secondsField.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, 0);
 }
